939dc839d9ea039c33cb0227e5b9cedd3f876151
Blandford Alex <alex.blandford@hetic.net>
2019-05-02 16:32:59 +0200

    added heatmap and colors

--- src/components/Map/MapContainer.js
+++ src/components/Map/MapContainer.js
@@ -10,6 +10,44 @@ const Map = ReactMapboxGl({
 
 const ES = new EatStreet("bcaacb4012b3e000");
 
+
+const layerPaint = {
+  'heatmap-weight': {
+    property: 'priceIndicator',
+    type: 'exponential',
+    stops: [[0, 0], [5, 2]]
+  },
+  // Increase the heatmap color weight weight by zoom level
+  // heatmap-ntensity is a multiplier on top of heatmap-weight
+  'heatmap-intensity': {
+    stops: [[0, 0], [5, 1.2]]
+  },
+  // Color ramp for heatmap.  Domain is 0 (low) to 1 (high).
+  // Begin color ramp at 0-stop with a 0-transparancy color
+  // to create a blur-like effect.
+  'heatmap-color': [
+    'interpolate',
+    ['linear'],
+    ['heatmap-density'],
+    0,
+    'rgba(33,102,172,0)',
+    0.25,
+    'rgba(103,169,207, .5)',
+    0.5,
+    'rgba(209,229,240, .5)',
+    0.8,
+    'rgba(253,219,199, .5)',
+    1,
+    'rgba(239,138,98, .5)',
+    2,
+    'rgba(150,60,23, .5)'
+  ],
+  // Adjust the heatmap radius by zoom level
+  'heatmap-radius': {
+    stops: [[0, 1], [5, 50]]
+  }
+};
+
 class MapContainter extends Component {
   state = {
     restaurants: [],
@@ -43,15 +81,12 @@ class MapContainter extends Component {
           width: "100vw"
         }}
       >
-      {this.state.restaurants.map((restaurant) => {
-        return(
-        <Marker key={restaurant.longitude} coordinates={[ restaurant.longitude, restaurant.latitude ]}>
-          <img src={pin} alt="map pin" style={{ height: '5rem', width: '5rem' }} />
-        </Marker>
-      )})}
-      <GeoJSONLayer
-        data={this.state.restaurants}
-      />
+      <Layer type='heatmap' paint={layerPaint}>
+        {this.state.restaurants.map((restaurant) => {
+          return(
+          <Feature key={restaurant.longitude} coordinates={[ restaurant.longitude, restaurant.latitude ]} />
+        )})}
+      </Layer>
       </Map>
     )
   }
